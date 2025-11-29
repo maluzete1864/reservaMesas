456c622a82133a33b8c03de31a3d865f4e3cb4c0
julietaaaaaaaaaaaa <julia.souza3@estudante.ifms.edu.br>
2025-11-29 00:16:03 -0400

    acabamento

--- reservas-mesas/src/components/Header.jsx
+++ reservas-mesas/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        🍷 ReservaMesas
+         ReservaMesas
       </div>
 
       <div className="menu-container">
